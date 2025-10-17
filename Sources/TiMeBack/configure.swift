2a7dc92c799d1ed59796b9c32396433dc9437db8
Thibault Sanclemente <84449794+DevTipsy@users.noreply.github.com>
2025-10-17 10:40:34 +0200

    ajout CORSconfiguration

--- Sources/TiMeBack/configure.swift
+++ Sources/TiMeBack/configure.swift
@@ -6,7 +6,7 @@ import FluentSQL
 
 public func configure(_ app: Application) async throws {
     app.routes.defaultMaxBodySize = "10mb"
-
+    
     // uncomment to serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.http.server.configuration.hostname = "0.0.0.0"
@@ -21,25 +21,60 @@ public func configure(_ app: Application) async throws {
     
     ContentConfiguration.global.use(encoder: encoder, for: .json)
     ContentConfiguration.global.use(decoder: decoder, for: .json)
-
-app.databases.use(DatabaseConfigurationFactory.mysql(
+    
+    app.databases.use(DatabaseConfigurationFactory.mysql(
         hostname: Environment.get("DATABASE_HOST") ?? "127.0.0.1",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? 3306,
         username: Environment.get("DATABASE_USERNAME") ?? "root",
         password: Environment.get("DATABASE_PASSWORD") ?? "",
         database: Environment.get("DATABASE_NAME") ?? "TiMeDatabase"
     ), as: .mysql)
-
     
-//    if let sql = app.db(.mysql) as? (any SQLDatabase) {
-//        sql.raw("SELECT 1").run().whenComplete { response in
-//            print(response)
-//        }
-//    } else {
-//        print("⚠️ Le driver SQL n'est pas disponible (cast vers SQLDatabase impossible)")
-//    }
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .custom("http://127.0.0.1:5500"),
+        allowedMethods: [.GET, .POST, .PUT, .DELETE, .OPTIONS],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin],
+        cacheExpiration: 120
+    )
     
-
+    let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
+    app.middleware.use(corsMiddleware)
+    
+    app.get("byType") { req -> Response in
+        let acceptHeader = req.headers["Accept"].first ?? ""
+        switch acceptHeader {
+            
+        case "application/json":
+            let userJson = try UserPublicDTO(from: User())
+            return try Response(
+                status: .ok,
+                body: .init(data: JSONEncoder().encode(userJson)))
+            
+        case "text/html":
+//            let user = try UserPublicDTO(from: User())
+            let userHtml = "<p> Bonjour! </p>"
+            return Response(
+                status: .ok,
+                body: .init(string: userHtml))
+            
+        default:
+            let defaultResponse = "Type non initialisé"
+            return Response(
+                status: .ok,
+                body: .init(string: defaultResponse))
+        }
+        
+    }
+    
+    //    if let sql = app.db(.mysql) as? (any SQLDatabase) {
+    //        sql.raw("SELECT 1").run().whenComplete { response in
+    //            print(response)
+    //        }
+    //    } else {
+    //        print("⚠️ Le driver SQL n'est pas disponible (cast vers SQLDatabase impossible)")
+    //    }
+    
+    
     // register routes
     try routes(app)
 }
